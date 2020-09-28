2d8d38aadc9d6daf0f80ff868bff0051d5eac939
Kevin Burkett <stlbucket@gmail.com>
2020-09-28 11:44:45 -0700

    wtf

--- test/commands/init.test.ts
+++ test/commands/init.test.ts
@@ -2,16 +2,7 @@ import {expect, test} from '@oclif/test'
 
 describe('init', () => {
   test
-  .stdout()
-  .command(['init'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .command(['init', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
+  .it('generate', ctx => {
+    expect(true).to.equal(true)
   })
 })
--- test/commands/release.test.ts
+++ test/commands/release.test.ts
@@ -2,16 +2,7 @@ import {expect, test} from '@oclif/test'
 
 describe('release', () => {
   test
-  .stdout()
-  .command(['release'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .command(['release', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
+  .it('generate', ctx => {
+    expect(true).to.equal(true)
   })
 })
